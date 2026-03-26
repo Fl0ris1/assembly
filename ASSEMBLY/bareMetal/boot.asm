73ecc5f49bcfb9e6bfd7f627b388c977b15be151
Fl0ris1 <149056628+Fl0ris1@users.noreply.github.com>
2026-03-26 08:42:16 +0100

    Add files via upload

--- ASSEMBLY/bareMetal/boot.asm
+++ ASSEMBLY/bareMetal/boot.asm
@@ -1,1 +1,25 @@
+[org 0x7c00]
 
+start:
+	cli
+	xor ax, ax
+	mov ss, ax
+	mov sp, 0x7c00
+	mov ds, ax
+	mov es, ax
+	sti
+
+	mov ah, 0x0e
+	mov al, 'M'
+	int 0x10
+
+	jmp $
+
+error:
+	mov ah, 0xe
+	mov al, 'E'
+	int 0x10
+	jmp $
+
+times 510 - ($ - $$) db 0
+dw 0xAA55
